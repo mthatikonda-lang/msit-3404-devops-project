378d1bc6d63d6446655dfda65d162d8c83c7115e
Thatikonda <mthatikonda@clarku.edu>
2025-11-18 14:42:48 -0500

    changed app.py

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,4 @@ def view_submissions():
     return render_template('submissions.html', submissions=submissions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host='0.0.0.0', port=5000, debug=True)
